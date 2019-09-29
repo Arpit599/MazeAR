86e2b570a6a0063850c7884ff202451889ae3c45
Arpit Bharadwaj <46400885+Arpit599@users.noreply.github.com>
2019-09-29 16:05:47 +0530

    Add files via upload

---
+++ Scripts/BallScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public GameObject plane;
+    public GameObject SpawnPoint;
+
+    private void Update()
+    {
+        if (transform.position.y < plane.transform.position.y - 10)
+            transform.position = SpawnPoint.transform.position;
+    }
+}
